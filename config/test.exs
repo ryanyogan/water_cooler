11135c9e3e826e85475147bbdf2613b155396d74
ryanyogan <ryanyogan@gmail.com>
2020-02-13 08:22:06 -0800

    fixed up the secrets a tad

--- config/test.exs
+++ config/test.exs
@@ -1,18 +1,9 @@
-use Mix.Config
+import Config
 
-# Configure your database
-config :water_cooler, WaterCooler.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "water_cooler_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :water_cooler, WaterCoolerWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
+
+import_config "test.secret.exs"
